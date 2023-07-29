62cc255fd0900068a03c84f0fc9f1e4634912b14
inatatsu <tatsuhiro.9699@gmail.com>
2023-07-29 17:48:28 +0900

    :sparkles:

--- functions/src/utils/Config.ts
+++ functions/src/utils/Config.ts
@@ -1,14 +1,3 @@
-// import * as functions from "firebase-functions";
-
-// interface IConfig {
-//   config: {
-//     serviceAccount: string;
-//     nodeUrl: string;
-//   };
-// }
-
-// export const Config = functions.config() as IConfig;
-
 export const Config = {
   config: {
     nodeUrl: process.env.NODE_URL,
